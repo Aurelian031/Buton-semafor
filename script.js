08a17bde40ef2bb41a2a253027823989e1ea0ec2
Nicolae Aurelian <nicolaeaurelian-31@gmail.com>
2023-04-25 18:31:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const interval = 10; // seconds
+
+function changeColor() {
+    const myButton = document.getElementById('myButton');
+    if (myButton.style.backgroundColor === 'red') {
+        myButton.style.backgroundColor = 'yellow';
+    } else if (myButton.style.backgroundColor === 'yellow') {
+        myButton.style.backgroundColor = 'green';
+    } else {
+        myButton.style.backgroundColor = 'red';
+    }
+}
+ setInterval(() => {
+    changeColor();
+    // const myButton = document.getElementById('myButton');
+    // if (myButton.style.backgroundColor === 'red') {
+    //     myButton.style.backgroundColor = 'yellow';
+    // } else if (myButton.style.backgroundColor === 'yellow') {
+    //     myButton.style.backgroundColor = 'green';
+    // } else {
+    //     myButton.style.backgroundColor = 'red';
+    // }
+}, interval * 1000);
